bb8684a477bba7d8a8d70b330cb888447d81a1f4
Roman Nasyrov <romnasi@gmail.com>
2020-12-02 13:50:25 +0300

    Добавляет стили -неоморфизм, добавляет сборку js на webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "./js/util.js",
+    "./js/canvas.js",
+    "./js/text.js",
+    "./js/save-png.js",
+    "./js/add-image.js",
+    "./js/clipboard.js",
+    "./js/modify-text.js",
+    "./js/robo.js",
+    "./js/tabs.js",
+    "./js/gradient.js"
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
+};
